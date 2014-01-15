10f75906c8902220b007a42785a302505b743795
Heikki Pora <heikki.pora@reaktor.fi>
2014-01-15 22:40:47 +0200

    Handle empty people response gracefully

--- src/main/java/com/drillsoft/highrise/domain/People.java
+++ src/main/java/com/drillsoft/highrise/domain/People.java
@@ -2,10 +2,11 @@ package com.drillsoft.highrise.domain;
 
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+import java.util.ArrayList;
 import java.util.List;
 
 @XmlRootElement
 public class People {
     @XmlElement(name = "person")
-    public List<Person> persons;
+    public List<Person> persons = new ArrayList<Person>();
 }
