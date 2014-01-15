84c57d8464169ecb5f2499925af7b5c4b2e7ab4b
Heikki Pora <heikki.pora@reaktor.fi>
2014-01-16 00:25:58 +0200

    Remove unused imports

--- src/main/java/com/drillsoft/highrise/domain/Person.java
+++ src/main/java/com/drillsoft/highrise/domain/Person.java
@@ -3,8 +3,6 @@ package com.drillsoft.highrise.domain;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
-import java.beans.Transient;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
